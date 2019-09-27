bec3f8165207e64b5c2e442166f1e910665b8030
AsuraHolosmith <54941303+AsuraHolosmith@users.noreply.github.com>
2019-09-27 16:33:26 -0400

    Add files via upload
    adding the plugin from tutorial 2 to the repo

---
+++ Plugin Test/Plugin Test/Wrapper.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "PluginSettings.h"
+#include "SimpleClass.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+	// Put functions here
+	PLUGIN_API int SimpleFunction();
+
+#ifdef __cplusplus
+}
+#endif
